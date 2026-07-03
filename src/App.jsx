48b3007bba239bc61e93c68077fcbaad966baddf
Adya Arun <168610405+Adyaarun@users.noreply.github.com>
2026-07-03 07:58:27 +0530

    Update import paths to use consistent casing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter} from "react-router-dom";
-import AdminComplaintList from "./components/AdminComplaintList";
-import StatusUpdateScreen from "./components/StatusUpdateScreen";
+import AdminComplaintList from "./Components/AdminComplaintList";
+import StatusUpdateScreen from "./Components/StatusUpdateScreen";
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
@@ -8,8 +8,8 @@ import heroImg from './assets/hero.png'
 import './App.css'
 import NavBar from './Components/NavBar'
 import { Route, Routes } from 'react-router-dom'
-import Login from './components/Login'
-import Register from './components/Register'
+import Login from './Components/Login'
+import Register from './Components/Register'
 import AddComplaint from './Components/AddComplaint'
 import ComplainList from './Components/ComplainList'
 import StudentDashboard from './Components/StudentDashboard'
